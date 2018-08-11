bab4f8cd9e2a40567cf8dfccdf14a44c992257fe
chamikab <kasun.chamika@gmail.com>
2018-08-11 22:02:51 +0530

    refactored code.

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavaigationItems/NavigationItems';
 import BackDrop from '../../UI/Backdrop/Backdrop';
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Aux/Aux';
 
 import classes from './SideDrawer.css';
 
