df767f2e73bfa6bd3ad33dc5f0f7d3078b5006ba
chamikab <kasun.chamika@gmail.com>
2018-08-11 22:04:54 +0530

    refactored code.

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,35 +1,31 @@
 import React from 'react';
-import classes from './BuildControls.css';
 
+import classes from './BuildControls.css';
 import BuildControl from './BuildControl/BuildControl';
 
-const controllers = [
-  {label: 'Salad', type : 'salad'},
-  {label: 'Bacon', type : 'bacon'},
-  {label: 'Cheese', type : 'cheese'},
-  {label: 'Meat', type : 'meat'},
+const controls = [
+    { label: 'Salad', type: 'salad' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Meat', type: 'meat' },
 ];
 
 const buildControls = (props) => (
-  <div className={classes.BuildControls}>
-    <p>Current Price : <strong>{Math.abs(props.totalPrice.toFixed(2))}</strong></p>
-    {
-      controllers.map(control => {
-        return <BuildControl
-          key={ control.label }
-          label={ control.label }
-          add={ () => props.addIngredinets(control.type) }
-          remove={ () =>  props.remvoeIngredinets(control.type) }
-          disabled={props.disabledInfo[control.type]}
-        />
-      })
-    }
-    <button
-      className={classes.OrderButton}
-      disabled={!props.purchasable}
-      onClick={props.ordered}
-    >ORDER NOW</button>
-  </div>
+    <div className={classes.BuildControls}>
+        <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
+        {controls.map(ctrl => (
+            <BuildControl 
+                key={ctrl.label} 
+                label={ctrl.label}
+                added={() => props.ingredientAdded(ctrl.type)}
+                removed={() => props.ingredientRemoved(ctrl.type)}
+                disabled={props.disabled[ctrl.type]} />
+        ))}
+        <button 
+            className={classes.OrderButton}
+            disabled={!props.purchasable}
+            onClick={props.ordered}>ORDER NOW</button>
+    </div>
 );
 
 export default buildControls;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,19 +2,19 @@ import React from 'react';
 
 import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
-import NavigationItems from '../NavaigationItems/NavigationItems';
+import NavigationItems from '../NavigationItems/NavigationItems';
 import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
-const toolBar = (props) => (
-  <header className={classes.Toolbar}>
-    <DrawerToggle clicked={props.drawerToggleClicked}/>
-    <div className={classes.Logo}>
-      <Logo/>
-    </div>
-    <nav className={classes.DesktopOnly}>
-     <NavigationItems/>
-    </nav>
-  </header>
+const toolbar = ( props ) => (
+    <header className={classes.Toolbar}>
+        <DrawerToggle clicked={props.drawerToggleClicked} />
+        <div className={classes.Logo}>
+            <Logo />
+        </div>
+        <nav className={classes.DesktopOnly}>
+            <NavigationItems />
+        </nav>
+    </header>
 );
 
-export default toolBar;
+export default toolbar;
