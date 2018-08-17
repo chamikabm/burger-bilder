203e4f280038ff4ad810e3cf44c6d52fd844c6e9
chamikab <kasun.chamika@gmail.com>
2018-08-18 00:47:21 +0530

    Check folder added.

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -6,19 +6,20 @@ import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 
 class Layout extends Component {
+
     state = {
         showSideDrawer: false
-    }
+    };
 
     sideDrawerClosedHandler = () => {
         this.setState( { showSideDrawer: false } );
-    }
+    };
 
     sideDrawerToggleHandler = () => {
         this.setState( ( prevState ) => {
             return { showSideDrawer: !prevState.showSideDrawer };
         } );
-    }
+    };
 
     render () {
         return (
