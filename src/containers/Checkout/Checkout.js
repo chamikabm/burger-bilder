af376d73c07f4d096f35a6d4bbb90fbdd72c3adb
chamikab <kasun.chamika@gmail.com>
2018-08-22 00:21:34 +0530

    Added changes related to basic routings.

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -12,11 +12,23 @@ class Checkout extends Component {
     }
   };
 
+  checkoutCancelHandler = () => {
+    this.props.history.goBack();
+  };
+
+  checkoutContinueHandler = () => {
+    this.props.history.replace('checkout/contact-data');
+  };
+
   render () {
 
     return (
       <div>
-        <CheckoutSummary ingredients={this.state.ingredients}/>
+        <CheckoutSummary
+          ingredients={this.state.ingredients}
+          checkoutCancelled={this.checkoutCancelHandler}
+          checkoutContinued={this.checkoutContinueHandler}
+        />
       </div>
     );
   };
