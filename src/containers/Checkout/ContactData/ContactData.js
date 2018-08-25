7f04d781531fa8de8bd03952d37dcaf3500557ac
chamikab <kasun.chamika@gmail.com>
2018-08-25 12:24:55 +0530

    Added changes to form

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
 
 import classes from './ContactData.css';
 import axios from "../../../axios-orders";
@@ -8,11 +9,58 @@ import axios from "../../../axios-orders";
 class ContactData extends Component {
 
   state = {
-    name: '',
-    email: '',
-    address: {
-      street: '',
-      postalCode: '',
+    orderForm: {
+      name: {
+        elementType : 'input',
+        elementConfig : {
+          type : 'text',
+          placeholder : 'Your Name',
+        },
+        value: ''
+      },
+      street: {
+        elementType : 'input',
+        elementConfig : {
+          type : 'text',
+          placeholder : 'Street',
+        },
+        value: ''
+      },
+      zipCode: {
+        elementType : 'input',
+        elementConfig : {
+          type : 'text',
+          placeholder : 'Zip Code',
+        },
+        value: ''
+      },
+      country: {
+        elementType : 'input',
+        elementConfig : {
+          type : 'text',
+          placeholder : 'Country',
+        },
+        value: ''
+      },
+      email: {
+        elementType : 'input',
+        elementConfig : {
+          type : 'email',
+          placeholder : 'Your E-mail Address',
+        },
+        value: ''
+      },
+      deliveryMethod: {
+        elementType : 'select',
+        elementConfig : {
+          options: [
+            {value : 'fastest', displayValue: 'Fastest'},
+            {value : 'cheapest', displayValue: 'Cheapest'},
+            {value : 'pickup', displayValue: 'Pickup'},
+          ],
+        },
+        value: ''
+      },
     },
     loading: false,
   };
@@ -21,45 +69,51 @@ class ContactData extends Component {
     event.preventDefault(); // This stops default behaviour such as reloading page upon clicking the button.
     console.log(this.props.ingredients);
 
-    this.setState({ loading : true });
+    this.setState({loading: true});
 
     const order = {
       ingredients: this.props.ingredients,
       price: this.props.price,
-      customer: {
-        name: 'Max Spend',
-        address: {
-          street: 'No 4, St Dkerke, Adddslkes.',
-          zipCode : '11838',
-          country: 'SriLanka'
-        },
-        email: 'abc@gmail.com',
-      },
+
       deliveryMethod: 'fastest',
     };
 
     axios.post('/orders.json', order)
       .then(response => {
-        this.setState({ loading: false });
+        this.setState({loading: false});
         this.props.history.push('/');
         console.log(response);
       })
       .catch(error => {
-        this.setState({ loading: false });
+        this.setState({loading: false});
         console.log(error);
       });
 
   };
 
   render() {
+    const formElementsArray = [];
+    for (let key in this.state.orderForm) {
+      formElementsArray.push({
+        id: key,
+        config: this.state.orderForm[key],
+      });
+    }
+
     let form =
       <form>
-      <input type="text" name="name" placeholder="Your Name"/>
-      <input type="text" name="email" placeholder="Your E-mail"/>
-      <input type="text" name="street" placeholder="Street"/>
-      <input type="text" name="postal" placeholder="Postal Code"/>
-      <Button btnType="Success" clicked={this.orderHandler} >ORDER</Button>
-    </form>;
+        {
+          formElementsArray.map(formElement => {
+            return <Input
+              key={formElement.id}
+              elementType={formElement.config.elementType}
+              elementConfig={formElement.config.elementConfig}
+              value={formElement.config.value}
+            />;
+          })
+        }
+        <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+      </form>;
 
     if (this.state.loading) {
       form = <Spinner/>;
