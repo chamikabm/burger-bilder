b551196aaf709c4d6a4999873afd79f25cafc7c1
chamikab <kasun.chamika@gmail.com>
2018-09-05 21:03:45 +0530

    Added routing guards for authenticated users.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch, withRouter } from 'react-router-dom';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import Layout from './hoc/Layout/Layout';
@@ -17,21 +17,42 @@ class App extends Component {
   }
 
   render() {
+
+    let routes = (
+      <Switch>
+        <Route path="/auth" component={Auth}/>
+        <Route path="/" component={BurgerBuilder}/>
+        <Redirect to='/'/> {/* Anything unknown will route back to main page */}
+      </Switch>
+    );
+
+    if (this.props.isAuthenticated) {
+      routes = (
+        <Switch>
+          <Route path="/checkout" component={Checkout}/>
+          <Route path="/orders" component={Orders}/>
+          <Route path="/logout" component={Logout}/>
+          <Route path="/" component={BurgerBuilder}/>
+          <Redirect to='/'/> {/* Anything unknown will route back to main page */}
+        </Switch>
+      );
+    }
+
     return (
       <div>
         <Layout>
-          <Switch>
-            <Route path="/auth" component={Auth}/>
-            <Route path="/checkout" component={Checkout}/>
-            <Route path="/orders" component={Orders}/>
-            <Route path="/logout" component={Logout}/>
-            <Route path="/" component={BurgerBuilder}/>
-          </Switch>
+          {routes}
         </Layout>
       </div>
     );
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null,
+  }
+};
 
 const mapDispatchToProps = dispatch => {
  return {
@@ -39,6 +60,6 @@ const mapDispatchToProps = dispatch => {
  }
 };
 
-// export default connect(null, mapDispatchToProps)(App);
-export default withRouter(connect(null, mapDispatchToProps)(App)); // We wrap this App connect with withRouter HOC as this connect does not pass the routing information with connect.
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App)); // We wrap this App connect with withRouter HOC as this connect does not pass the routing information with connect.
 // This withRouter will make sure that routing props will pass down to the components.
