e7ef6e215e8edfd2fa76c331207aa2c69d1ca5cd
chamikab <kasun.chamika@gmail.com>
2018-09-07 00:05:24 +0530

    Added nvigation item test.

--- src/components/Navigation/NavigationItems/test/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/test/NavigationItems.test.js
@@ -7,9 +7,31 @@ import NavigationItem from '../NavigationItem/NavigationItem';
 
 configure({adapter: new Adapter()});
 
-describe('<NavigationItems/>', () => {
+// Method 1
+describe(' <NavigationItems/>', () => {
   it('Should render two <NavigationItem/> elements if not authenticated.', () => {
     const wrapper = shallow(<NavigationItems/>);
     expect(wrapper.find(NavigationItem)).toHaveLength(2);
+  });
+
+  it('Should render three <NavigationItem/> elements if authenticated.', () => {
+    const wrapper = shallow(<NavigationItems isAuthenticated/>);
+    expect(wrapper.find(NavigationItem)).toHaveLength(3);
+  });
+});
+
+// Method 2
+describe('<NavigationItems/>', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<NavigationItems/>);
+  });
+  it('Should render two <NavigationItem/> elements if not authenticated.', () => {
+    expect(wrapper.find(NavigationItem)).toHaveLength(2);
+  });
+
+  it('Should render three <NavigationItem/> elements if authenticated.', () => {
+    wrapper.setProps({isAuthenticated: true});
+    expect(wrapper.find(NavigationItem)).toHaveLength(3);
   });
 });
