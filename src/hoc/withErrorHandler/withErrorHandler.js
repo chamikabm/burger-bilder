643d5c48d239af03ebebdb9e8b86fcde0aff4bd2
chamikab <kasun.chamika@gmail.com>
2018-09-06 23:00:21 +0530

    Added async loading changes.

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import Modal from '../../components/UI/Modal/Modal';
 import Aux from '../Aux/Aux';
 
-const withErrorHandler= (WrappedComponent, axios) => {
+const withErrorHandler = (WrappedComponent, axios) => {
 
   return class extends Component {
 
